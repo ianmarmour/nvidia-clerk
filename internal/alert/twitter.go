18a24e6c35c5bd59aab4c667757a9f1272288dd6
Ian Armour <ianmarmour@gmail.com>
2020-09-22 21:49:17 -0700

    changed up some styling in SendTweet

--- internal/alert/twitter.go
+++ internal/alert/twitter.go
@@ -2,7 +2,6 @@ package alert
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
@@ -11,21 +10,13 @@ import (
 
 //SendTweet Sends an Tweet.
 func SendTweet(item string, config config.TwitterConfig) error {
-	oauthConfig := oauth1.NewConfig(config.ConsumerKey, config.ConsumerSecret)
+	oauth := oauth1.NewConfig(config.ConsumerKey, config.ConsumerSecret)
 	token := oauth1.NewToken(config.AccessToken, config.AccessSecret)
-	httpClient := oauthConfig.Client(oauth1.NoContext, token)
-	client := twitter.NewClient(httpClient)
+	http := oauth.Client(oauth1.NoContext, token)
+	twitter := twitter.NewClient(http)
 
-	verifyParams := &twitter.AccountVerifyParams{
-		SkipStatus:   twitter.Bool(true),
-		IncludeEmail: twitter.Bool(true),
-	}
-	user, _, _ := client.Accounts.VerifyCredentials(verifyParams)
-	fmt.Printf("User's ACCOUNT:\n%+v\n", user)
-
-	_, _, err := client.Statuses.Update(fmt.Sprintf("%s is now in stock on the NVIDIA Store", item), nil)
+	_, _, err := twitter.Statuses.Update(fmt.Sprintf("%s is now in stock on the NVIDIA Store", item), nil)
 	if err != nil {
-		log.Println("Error attempting to Tweet")
 		return err
 	}
 
